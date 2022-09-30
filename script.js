f944f5497e82a9044b3d8987c00e1914ad92069f
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-09-30 16:04:26 +0200

    add constant for the game

---
+++ script.js
@@ -0,0 +1,9 @@
+// constante du jeu //
+let score = [0, 0];
+let round = 0;
+let playerTurn = 0;
+let gameOn = true;
+
+const game = document.getElementById('btn-game');
+const roll = document.getElementById('btn-roll');
+const hold = document.getElementById('btn-hold');
