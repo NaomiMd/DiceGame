c86773ab86ad55a141ccb7d50f7a606a1ac19362
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-09-30 16:53:18 +0200

    add game button function

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ let gameOn = true;
 const game = document.getElementById('btn-game');
 const roll = document.getElementById('btn-roll');
 const hold = document.getElementById('btn-hold');
+
+// bouton game | on met tous les scores à 0 //
+game.addEventListener('click', ()=>{
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.box-1').classList.remove('active');
+    document.querySelector('.box-0').classList.add('active');
+});
+// bouton roll //
+
+// bouton hold //
