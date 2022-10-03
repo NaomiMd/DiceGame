aa908c8d7b583c783825399de4724cf9f7f753b0
Naomi Maindret <naomi.maindret@hotmail.fr>
2022-10-03 13:27:04 +0200

    remove comments

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// constante du jeu //
 let score = [0, 0];
 let round = 0;
 let playerTurn = 0;
@@ -8,7 +7,6 @@ const game = document.getElementById('btn-game');
 const roll = document.getElementById('btn-roll');
 const hold = document.getElementById('btn-hold');
 
-// bouton game | on met tous les scores à 0 //
 game.addEventListener('click', ()=>{
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
@@ -18,13 +16,11 @@ game.addEventListener('click', ()=>{
     document.querySelector('.box-1').classList.remove('active');
     document.querySelector('.box-0').classList.add('active');
 });
-// bouton roll //
 roll.addEventListener('click', ()=>{
     if(gameOn){
     const dice = Math.floor(Math.random() * 6) + 1;
     const diceImg = "img/"+"dice-"+dice+".png";
     document.querySelectorAll("img")[0].setAttribute('src', diceImg);
-
     if(dice !==1){
         round += dice;
         document.querySelector(`#current-${playerTurn}`).textContent = round;
@@ -33,7 +29,6 @@ roll.addEventListener('click', ()=>{
     }
    }
 });
-// bouton hold //
 hold.addEventListener('click', ()=>{
     if(gameOn){
     score[playerTurn] += round;
@@ -47,13 +42,11 @@ hold.addEventListener('click', ()=>{
     }
    }
 });
-
 let otherPlayer = ()=>{
-    playerTurn === 0 ? playerTurn =1 : playerTurn = 0;
+    playerTurn === 0 ? playerTurn = 1 : playerTurn = 0;
     round = 0;
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
     document.querySelector('.box-0').classList.toggle('active');
     document.querySelector('.box-1').classList.toggle('active');
-
 }
